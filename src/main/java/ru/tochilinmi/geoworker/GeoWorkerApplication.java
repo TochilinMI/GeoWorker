7ca9862d549e8f2f85c551214f0911c7cb4fc2c4
TochilinMI <tochilin.m.i@mail.ru>
2022-11-01 18:09:51 +0300

    Init commit

---
+++ src/main/java/ru/tochilinmi/geoworker/GeoWorkerApplication.java
@@ -0,0 +1,13 @@
+package ru.tochilinmi.geoworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tochilinmi/geoworker/GeoWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tochilinmi.geoworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
