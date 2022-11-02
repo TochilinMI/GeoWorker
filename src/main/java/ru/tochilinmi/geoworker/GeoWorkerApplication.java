b62c0a76e858c57ee4ae5733e048438e5d7a7b84
TochilinMI <tochilin.m.i@mail.ru>
2022-11-02 11:20:12 +0300

    Use include method Polygon

--- src/main/java/ru/tochilinmi/geoworker/GeoWorkerApplication.java
+++ src/main/java/ru/tochilinmi/geoworker/GeoWorkerApplication.java
@@ -3,11 +3,20 @@ package ru.tochilinmi.geoworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+
 @SpringBootApplication
 public class GeoWorkerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GeoWorkerApplication.class, args);
-    }
+        int[] x = new int[] {1,100,1,100,50};
+        int[] y = new int[] {1,100,100,1,150};
 
+        int planeX = 50;
+        int planeY = 120;
+
+        int poly = new Polygon(x,y,x.length).contains(new Point(planeX, planeY))?1:2;
+        System.out.println(poly);
+    }
 }
