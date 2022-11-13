e27a98f259380eb363bf908aa5cba37263a9714d
TochilinMI <tochilin.m.i@mail.ru>
2022-11-13 11:30:06 +0300

    Measure between couple points

--- src/main/java/ru/tochilinmi/geoworker/GeoWorkerApplication.java
+++ src/main/java/ru/tochilinmi/geoworker/GeoWorkerApplication.java
@@ -33,4 +33,16 @@ public class GeoWorkerApplication {
         PointInPolygon poly = new PointInPolygon();
         System.out.println(poly.pointInPolygon(coord,point));
     }
+    public static Double measure(Double lat1,Double lon1,Double lat2,Double lon2){
+        Double r = 6378.137; // Radius of earth in KM
+        Double dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+        Double dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+        Double a = Math.pow(Math.sin(dLat/2),2)
+                + Math.cos(lat1*Math.PI/180)
+                *  Math.cos(lat2*Math.PI/180)
+                * Math.pow(Math.sin(dLon/2),2);
+        Double c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+        Double d = r * c;
+        return d * 1000;
+    }
 }
